5d5acf7727eb28719ac3d0c589a329895c986ba8
barg <bar172004@gmail.com>
2024-08-15 11:42:53 +0300

    added connection object to the mongo database and .env file including the db url

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
+import { mongoString } from './configs/db.cnfig.js';
 import { json, urlencoded } from 'express';
+import mongoose from 'mongoose';
 import express from 'express';
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +20,13 @@ app.get('/', (req, res) => {
   res.json({'message': 'ok'});
 })
 
+database.on('error', (error) => {
+  console.log(error)
+})
+
+database.once('connected', () => {
+  console.log('Database Connected');
+})
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening at http://localhost:${port}`)
